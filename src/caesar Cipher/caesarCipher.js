1cbfafa5b0f0dd13a509580d0da738f5149c0337
s0ur4bhkumar <soul65917@gmail.com>
2026-02-12 10:48:13 +0530

    started working on caesar cipher

---
+++ src/caesar Cipher/caesarCipher.js
@@ -0,0 +1,11 @@
+const word = 'Hello'
+let code = word.charCodeAt(0)
+let chr = String.fromCharCode(code + 3)
+console.log(code)
+console.log(chr)
+
+function caesarCipher(word) {
+  
+}
+
+export default caesarCipher
---
+++ src/caesar Cipher/caesarCipher.spec.js
@@ -0,0 +1,13 @@
+import caesarCipher from "./caesarCipher.js";
+
+test('test for wrapping from z to a', () => {
+  expect(caesarCipher('xyz',3)).toBe('abc')
+})
+
+test('test for case preservation', () => {
+  expect(caesarCipher('heLLo',3)).toBe('khOOr')
+})
+
+test('punctuation,spaces and other non-alphabetical should remain unchanged', () => {
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!')
+})
